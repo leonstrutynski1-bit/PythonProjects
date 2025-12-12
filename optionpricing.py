5c948db9eec8736caf01933bd7bb16c842dfb33d
leonstrutynski1_bit <leon.strutynski.1@gmail.com>
2025-12-12 13:50:39 -0500

    Test to see if commit work

--- optionpricing.py
+++ optionpricing.py
@@ -1,5 +1,5 @@
-#Building a python code for option pricing with the Black-Scholes formula. 
-
+# Building a python code for option pricing with the Black-Scholes formula. 
+# Test to see if commit on github works
 import yfinance as yf
 import numpy as np
 import pandas as pd
